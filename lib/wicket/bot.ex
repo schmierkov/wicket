3b03310ff018b2cda3d3f06a15f42eea1264608d
Sirko Sittig <sirko.sittig@gmail.com>
2018-02-27 22:41:08 -0300

    io output cleanup

--- lib/wicket/bot.ex
+++ lib/wicket/bot.ex
@@ -2,7 +2,7 @@ defmodule Wicket.Bot do
   use Slack
 
   def handle_connect(slack, state) do
-    IO.puts "Connected as #{slack.me.name}"
+    # IO.puts "Connected as #{slack.me.name}"
     {:ok, state}
   end
 
@@ -15,8 +15,6 @@ defmodule Wicket.Bot do
   def handle_event(_, _, state), do: {:ok, state}
 
   def handle_info({:message, text, channel}, slack, state) do
-    IO.puts "Sending your message, captain!"
-
     send_message(text, channel, slack)
 
     {:ok, state}
